f722514b67bb51dc3e2b77bd4f5720dd8d98795b
nikos0406 <107640882+nikos0406@users.noreply.github.com>
2024-02-14 13:06:22 +0100

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Nikos Kounakas on 14.02.24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
