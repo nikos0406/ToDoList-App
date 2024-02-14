49437e97293f103dab18efe9f2dcbbe8dbbdde64
nikos0406 <107640882+nikos0406@users.noreply.github.com>
2024-02-14 17:36:08 +0100

    Finished LoginView. LoginView is the first thing that shows up when opening the app.

--- ToDo/Views/LoginView.swift
+++ ToDo/Views/LoginView.swift
@@ -8,13 +8,44 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
         VStack{
             // Header
             HeaderView()
+            
             // Login Form
+            Form{
+                TextField("Email Address", text: $email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                SecureField("Password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                Button{
+                    // attempt log in
+                } label : {
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                        
+                        Text("Log In")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                }
+            }
             
             // Create Account
+            VStack{
+                Text("New around here?")
+                Button("Create An Account"){
+                    // Show registration
+                }
+            }
+            .padding(.bottom, 50)
             
             Spacer()
         }
