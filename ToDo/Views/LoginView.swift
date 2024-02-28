d1b2c09a41692cbbba6c3561d9d11a2af93342f4
nikos0406 <107640882+nikos0406@users.noreply.github.com>
2024-02-28 16:12:01 +0100

    Added business logic in LoginViewViewModel class. We are now able to check if all fields are filled out and if an email ist valid. It is also possible to add custom error messages.

--- ToDo/Views/LoginView.swift
+++ ToDo/Views/LoginView.swift
@@ -8,9 +8,7 @@
 import SwiftUI
 
 struct LoginView: View {
-    
-    @State var email = ""
-    @State var password = ""
+    @StateObject var viewModel = LoginViewViewModel()
     
     var body: some View {
         NavigationView{
@@ -23,18 +21,24 @@ struct LoginView: View {
                 
                 // Login Form
                 Form{
-                    TextField("Email Address", text: $email)
+                    
+                    if !viewModel.errorMessage.isEmpty{
+                        Text(viewModel.errorMessage)
+                            .foregroundColor(.red)
+                    }
+                    
+                    TextField("Email Address", text: $viewModel.email)
                         .textFieldStyle(DefaultTextFieldStyle())
                         .textInputAutocapitalization(.none)
                         .autocorrectionDisabled()
                     
-                    SecureField("Password", text: $password)
+                    SecureField("Password", text: $viewModel.password)
                         .textFieldStyle(DefaultTextFieldStyle())
                     
                     TLButton(text: "Log In", 
                              color: .blue
                     ) {
-                        //Attempt Log In
+                        viewModel.login()
                     }
                     
                 }
